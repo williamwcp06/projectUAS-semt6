11a36ce7f3736cfcb3e55ebaba9f681ff908d2a2
iosdev <iosdev@labb611-06.local>
2017-04-29 10:29:46 +0700

    Initial Commit

---
+++ Project UAS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project UAS
+//
+//  Created by iosdev on 4/29/17.
+//  Copyright © 2017 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
